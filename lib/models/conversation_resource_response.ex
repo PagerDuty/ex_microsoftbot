d8060d2cec5ad7a4c3d6949f68eae5d375cc5cae
Daria Struts <daryastruts@gmail.com>
2020-05-05 16:25:03 +0300

    Add ConversationResourceResponce for create_conversation api (#7)

---
+++ lib/models/conversation_resource_response.ex
@@ -0,0 +1,35 @@
+defmodule ExMicrosoftBot.Models.ConversationResourceResponse do
+  @moduledoc """
+  Microsoft bot conversation API resource response
+  """
+
+  @derive [Poison.Encoder]
+  defstruct [:id, :activityId, :serviceUrl]
+
+  @type t :: %ExMicrosoftBot.Models.ConversationResourceResponse{
+    id: String.t,
+    activityId: String.t,
+    serviceUrl: String.t
+  }
+
+  @doc """
+  Decode a map into `ExMicrosoftBot.Models.ConversationResourceResponse`
+  """
+  @spec parse(map) :: {:ok, ExMicrosoftBot.Models.ConversationResourceResponse.t}
+  def parse(param) when is_map(param) do
+    {:ok, Poison.Decode.decode(param, as: decoding_map())}
+  end
+
+  @doc """
+  Decode a string into `ExMicrosoftBot.Models.ConversationResourceResponse`
+  """
+  @spec parse(String.t) :: ExMicrosoftBot.Models.ConversationResourceResponse.t
+  def parse(param) when is_binary(param) do
+    Poison.decode!(param, as: decoding_map())
+  end
+
+  @doc false
+  def decoding_map() do
+    %ExMicrosoftBot.Models.ConversationResourceResponse{}
+  end
+end
