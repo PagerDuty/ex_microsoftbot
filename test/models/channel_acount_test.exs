11dbab79a5aca4004e8f9e6465cda116e821c302
Viktor Klymentiev <viclime@gmail.com>
2020-08-06 11:28:08 +0300

    Added aadObjectId to ChannelAccount model (#8)

--- test/models/channel_acount_test.exs
+++ test/models/channel_acount_test.exs
@@ -5,37 +5,45 @@ defmodule ExMicrosoftBot.Models.ChannelAcountTest do
 
   describe ".parse/1" do
     test "parses a map into the right struct" do
-      {:ok, %ChannelAccount{id: id, name: name}} = ChannelAccount.parse(%{
-        "id" => "123456789",
-        "name" => "Some Name"
-      })
+      {:ok, %ChannelAccount{id: id, name: name, aadObjectId: aad_object_id}} =
+        ChannelAccount.parse(%{
+          "id" => "123456789",
+          "name" => "Some Name",
+          "aadObjectId" => "00000000-0000-0000-0000-000000000000"
+        })
 
       assert id == "123456789"
       assert name == "Some Name"
+      assert aad_object_id == "00000000-0000-0000-0000-000000000000"
     end
 
     test "parses a list into an array of the right struct" do
-      {:ok, result} = ChannelAccount.parse([
-        %{
-          "id" => "123456789",
-          "name" => "Some Name"
-        },
-        %{
-          "id" => "987654321",
-          "name" => "Some Other Name"
-        }
-      ])
+      {:ok, result} =
+        ChannelAccount.parse([
+          %{
+            "id" => "123456789",
+            "name" => "Some Name",
+            "aadObjectId" => "00000000-0000-0000-0000-000000000000"
+          },
+          %{
+            "id" => "987654321",
+            "name" => "Some Other Name",
+            "aadObjectId" => "11111111-1111-1111-1111-111111111111"
+          }
+        ])
 
       [
-        %ChannelAccount{id: id_a, name: name_a},
-        %ChannelAccount{id: id_b, name: name_b}
+        %ChannelAccount{id: id_a, name: name_a, aadObjectId: aad_object_id_a},
+        %ChannelAccount{id: id_b, name: name_b, aadObjectId: aad_object_id_b}
       ] = result
 
       assert id_a == "123456789"
       assert name_a == "Some Name"
+      assert aad_object_id_a == "00000000-0000-0000-0000-000000000000"
 
       assert id_b == "987654321"
       assert name_b == "Some Other Name"
+      assert aad_object_id_b == "11111111-1111-1111-1111-111111111111"
     end
 
     test "parses a JSON encoded object" do
@@ -47,21 +55,24 @@ defmodule ExMicrosoftBot.Models.ChannelAcountTest do
     end
 
     test "parses a JSON encoded array of objects" do
-      result = ChannelAccount.parse(
-        "[{\"id\":\"123456789\",\"name\":\"Some Name\"}," <>
-        "{\"id\":\"987654321\",\"name\":\"Some Other Name\"}]"
-      )
+      result =
+        ChannelAccount.parse(
+          "[{\"id\":\"123456789\",\"name\":\"Some Name\",\"aadObjectId\":\"00000000-0000-0000-0000-000000000000\"}," <>
+            "{\"id\":\"987654321\",\"name\":\"Some Other Name\",\"aadObjectId\":\"11111111-1111-1111-1111-111111111111\"}]"
+        )
 
       [
-        %ChannelAccount{id: id_a, name: name_a},
-        %ChannelAccount{id: id_b, name: name_b}
+        %ChannelAccount{id: id_a, name: name_a, aadObjectId: aad_object_id_a},
+        %ChannelAccount{id: id_b, name: name_b, aadObjectId: aad_object_id_b}
       ] = result
 
       assert id_a == "123456789"
       assert name_a == "Some Name"
+      assert aad_object_id_a == "00000000-0000-0000-0000-000000000000"
 
       assert id_b == "987654321"
       assert name_b == "Some Other Name"
+      assert aad_object_id_b == "11111111-1111-1111-1111-111111111111"
     end
   end
 end
